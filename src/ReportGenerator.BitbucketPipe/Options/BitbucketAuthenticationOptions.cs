87324143880ad9a331318822d0e20369b9e74dd4
LoremFooBar <loremfoobardev@gmail.com>
2023-08-03 18:51:20 +0300

    fix: nullable error

--- src/ReportGenerator.BitbucketPipe/Options/BitbucketAuthenticationOptions.cs
+++ src/ReportGenerator.BitbucketPipe/Options/BitbucketAuthenticationOptions.cs
@@ -1,8 +1,12 @@
-﻿namespace ReportGenerator.BitbucketPipe.Options;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ReportGenerator.BitbucketPipe.Options;
 
 public class BitbucketAuthenticationOptions
 {
     public string? Username { get; set; }
     public string? AppPassword { get; set; }
+
+    [MemberNotNullWhen(true, nameof(Username), nameof(AppPassword))]
     public bool UseAuthentication => !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(AppPassword);
 }
